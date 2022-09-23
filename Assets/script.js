4d840f851fc90457089e7e6814fe108c94da731f
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-23 14:41:57 -0700

    hid time and fixed up functionlity

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
+//This displays the current date
 var today = moment();
 $("#todays-date").text(today.format("MMM Do, YYYY"));
 
+//This function extracts the data from open weather
 let weather = {
     "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
     fetchWeather: function (city) {
@@ -11,20 +13,37 @@ let weather = {
        .then((response) => response.json())
        .then((data) => this.displayWeather(data));
     },
+    //This Function retrieves the data then displays it to its respective locations on the page 
     displayWeather: function (data) {
         const {name} = data;
         const {icon} = data.weather[0];
         const {temp} = data.main;
         const {humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, temp, humidity, speed)
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-    }
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
 
+//This creates a function to listen fo the click on the search button 
+//Which then goes into the VAR "Weather" and executes the "search" function above
+document.querySelector(".search-btn").addEventListener("click", function () {
+    weather.search ();
+});
 
-// weather.fetchWeather("tucson")
+
+/**  fix this **/
+//This is an extra feature that allow you to press enter when you search instead of having to press the search button every time 
+//We add an event listener to "keyup" and give it a function of an "event"
+//"If" the button pressed is equal to "Enter" then run the search function
+// document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+//     if (event.key == "Enter"); {
+//         weather.search();
+//     }
+// });
