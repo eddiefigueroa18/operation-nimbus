b7ff10bc06b84c8cea5305c1e1e01bc848a42807
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-23 12:24:55 -0700

    added begining stages of weather dashboard. Added the API key, some html, css, and search bar

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var today = moment();
+$("#todays-date").text(today.format("MMM Do, YYYY"));
+
+let weather = {
+    "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8"
+    fetchWeather: function (){
+       fetch 
+    }
+};
