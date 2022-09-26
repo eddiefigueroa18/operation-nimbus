593434b718e0bf54febefb09449121cd8d5a3e00
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-26 12:49:01 -0700

    added the onload feature for current weather. Now need to work on the forecast to populate

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,45 @@ var today = moment();
 $("#todays-date").text(today.format("MMM Do, YYYY"));
 
 
-//------------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------------//
 
+//When page first load it already has a city to show the weather from 
+window.onload = function() { 
+    startLoad.loadWeather ()}; 
+
+let startLoad = {
+    "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
+    loadWeather: function (seattle) {
+       fetch ("https://api.openweathermap.org/data/2.5/weather?q=seattle" 
+        + "&units=imperial&appid=" 
+        + this.apiKey)
+       .then((response) => response.json())
+       .then((data) => this.onLoadWeather(data));
+    },
+    //This Function retrieves the data then displays it to its respective locations on the page 
+    onLoadWeather: function (data) {
+        const {name} = data;
+        const {icon} = data.weather[0];
+        const {temp} = data.main;
+        const {humidity} = data.main;
+        const {speed} = data.wind;
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    },
+};
+
+
+//-------------------------------------------------------------------------------------------------//
+//This creates a function to listen fo the click on the search button 
+//Which then goes into the VAR "currentWeather" and executes the "search" function above
+
+document.querySelector(".search-btn").addEventListener("click", function () {
+    currentWeather.search ();
+    forecast.search2 ();
+});
 //This function makes the call to the API
 //Then runs the function to display it
 let currentWeather = {
@@ -40,58 +77,47 @@ let currentWeather = {
 //Which then goes into the VAR "currentWeather" and executes the "search" function above
 document.querySelector(".search-btn").addEventListener("click", function () {
     currentWeather.search ();
-    // forecast.search2 ();
+    forecast.search2 ();
 });
 
 
-
-
-/**  fix this **/
-//This is an extra feature that allow you to press enter when you search instead of having to press the search button every time 
-//We add an event listener to "keyup" and give it a function of an "event"
-//"If" the button pressed is equal to "Enter" then run the search function
-// document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-//     if (event.key == "Enter"); {
-//         weather.search();
-//     }
-// });
-
-
-
-
-//------------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------------------------//
 // 5-day weather forecast section
+let forecast = {
+    "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
+    fetchForecast: function (city) {
+        fetch ("https://api.openweathermap.org/data/2.5/forecast?q="
+        + city
+        + "&units=imperial&appid="
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayForecast(data));
+    },
 
-// let forecast = {
-//     "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
-//     fetchForecast: function (city) {
-//         fetch ("https://api.openweathermap.org/data/2.5/forecast?q="
-//         + city
-//         + "&units=imperial&appid="
-//         + this.apiKey)
-//         .then((response) => response.json())
-//         .then((data) => this.displayForecast(data));
-//     },
+    displayForecast: function (data) {
+        for (i=0; i<5; i++) {
+            const {date} = data.dt_text;
+            const {icon} = data.weather[0].icon;
+            const {temp} = data.main;
+            const {humidity} = data.main;
+            const {speed} = data.wind;
 
-//     displayForecast: function (data) {
-//         for (i=0; i<5; i++) {
-//             const {date} = data.dt;
-//             const {icon} = data.weather[0].icon;
-//             const {temp} = data.main;
-//             const {humidity} = data.main;
-//             const {speed} = data.wind;
+            document.querySelector(".fDate" + i).innerText = date;
+            document.querySelector(".icon" + i).src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+            document.querySelector(".fTemp" + i).innerText = temp + "°F";
+            document.querySelector(".fWind" + i).innerText = "Wind Speed: " + speed + " mph";
+            document.querySelector(".fHumidity" + i).innerText = "Humidity: " + humidity + "%";
+        };
+    },
+    search2: function () {
+        this.fetchForecast(document.querySelector(".search-bar").value);
+    },
+};
 
-//             document.querySelector(".fDate" + i).innerText = date;
-//             document.querySelector(".icon" + i).src = "https://openweathermap.org/img/wn/"+ icon + ".png";
-//             document.querySelector(".fTemp" + i).innerText = temp + "°F";
-//             document.querySelector(".fWind" + i).innerText = "Wind Speed: " + speed + " mph";
-//             document.querySelector(".fHumidity" + i).innerText = "Humidity: " + humidity + "%";
-//         };
-//     },
-//     search2: function () {
-//         this.fetchForecast(document.querySelector(".search-bar").value);
-//     },
-// };
+
+
+//-----------------------------------------------------------------------------------------------------------//
+
 
 const searchBar = document.querySelector("#searchBar") //cityInput
 const submitbtn = document.querySelector("#submit"); //submitBtn
@@ -126,3 +152,14 @@ function searchHistory() {
 
         // recentSearchBtn.addEventListener("click", currentWeather)
     }
+
+
+    /**  fix this **/
+//This is an extra feature that allow you to press enter when you search instead of having to press the search button every time 
+//We add an event listener to "keyup" and give it a function of an "event"
+//"If" the button pressed is equal to "Enter" then run the search function
+// document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+//     if (event.key == "Enter"); {
+//         weather.search();
+//     }
+// });
