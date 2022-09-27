d1e8302bf53450e225ff848ec9c1b3ed75f049cd
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-26 17:27:19 -0700

    finally got the forecast to display. Need to still work on the local storage part

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,16 @@
 //This displays the current date
 var today = moment();
 $("#todays-date").text(today.format("MMM Do, YYYY"));
+// var apiKey = "fe350fcf1bdf0b9498e33816a7a5fca8"
+var forecasts = document.querySelectorAll(".forecasts");
 
-
-//-----------------------------------------------------------------------------------------//
-
+//--------------------------------------------------------------------------------------------------------------------//
+//ON-LOAD
 //When page first load it already has a city to show the weather from 
 window.onload = function() { 
-    startLoad.loadWeather ()}; 
-
+    startLoad.loadWeather ()
+   onForecast.onfetchForecast() 
+} 
 let startLoad = {
     "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
     loadWeather: function (seattle) {
@@ -33,17 +35,56 @@ let startLoad = {
     },
 };
 
+let onForecast = {
+    "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
+    onfetchForecast: function (seattle) {
+        fetch ("https://api.openweathermap.org/data/2.5/forecast?q=seattle"
+        + "&appid="
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayForecast(data));
+    },
 
-//-------------------------------------------------------------------------------------------------//
-//This creates a function to listen fo the click on the search button 
-//Which then goes into the VAR "currentWeather" and executes the "search" function above
+    onDisplayForecast: function (data) {
+        console.log(data)
+        for (i = 0; i < forecasts.length; i++) {
+            forecasts[i].innerText = "";
+            var forecastTime = i * 8 + 4;
+            var fDate = new Date(data.list[forecastTime].dt * 1000);
+            var fDay = fDate.getDate();
+            var fMonth = fDate.getMonth() + 1;
+            var fYear = fDate.getFullYear();
+            var forecastInfo = document.createElement("h4");
+                forecastInfo.innerText = fMonth + "/" + fDay + "/" + fYear;
+                forecasts[i].append(forecastInfo);
 
-document.querySelector(".search-btn").addEventListener("click", function () {
-    currentWeather.search ();
-    forecast.search2 ();
-});
-//This function makes the call to the API
-//Then runs the function to display it
+        var fIcon = document.createElement("img");
+        fIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastTime].weather[0].icon + "@2x.png");
+        forecasts[i].append(fIcon);
+        
+
+        var fTemp = document.createElement("p");
+        fTemp.innerHTML = "Temp: " + (Math.round(((data.list[forecastTime].main.temp - 273.15) * 9 / 5 + 32) * 100) / 100) + "°F";
+        forecasts[i].append(fTemp);
+        
+
+        var fWind = document.createElement("p");
+        fWind.innerHTML = "Wind: " + (Math.round(2.23694 * data.list[forecastTime].wind.speed * 100) / 100) + " MPH";
+        forecasts[i].append(fWind);
+
+        var fHumidity = document.createElement("p");
+        fHumidity.innerHTML = "Humidity: " + data.list[forecastTime].main.humidity + " %";
+        forecasts[i].append(fHumidity);
+        }
+    }
+}
+
+
+
+//---------------------------------------------------------------------------------------------------------------------//
+//CURRENT WEATHER
+// //This function makes the call to the API
+// //Then runs the function to display it
 let currentWeather = {
     "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
     fetchWeather: function (city) {
@@ -74,40 +115,59 @@ let currentWeather = {
 
 
 //This creates a function to listen fo the click on the search button 
-//Which then goes into the VAR "currentWeather" and executes the "search" function above
+//Which then goes into the VAR "currentWeather" and "forecast" then executes the "search" functions 
 document.querySelector(".search-btn").addEventListener("click", function () {
     currentWeather.search ();
     forecast.search2 ();
 });
 
 
-//---------------------------------------------------------------------------------------------------------//
-// 5-day weather forecast section
+
+//--------------------------------------------------------------------------------------------------------------------//
+// 5-DAY FORECAST
 let forecast = {
     "apiKey": "fe350fcf1bdf0b9498e33816a7a5fca8",
     fetchForecast: function (city) {
         fetch ("https://api.openweathermap.org/data/2.5/forecast?q="
         + city
-        + "&units=imperial&appid="
+        + "&appid="
         + this.apiKey)
         .then((response) => response.json())
         .then((data) => this.displayForecast(data));
     },
 
     displayForecast: function (data) {
-        for (i=0; i<5; i++) {
-            const {date} = data.dt_text;
-            const {icon} = data.weather[0].icon;
-            const {temp} = data.main;
-            const {humidity} = data.main;
-            const {speed} = data.wind;
+        console.log(data)
+        for (i = 0; i < forecasts.length; i++) {
+            forecasts[i].innerText = "";
+            var forecastTime = i * 8 + 4;
+            var fDate = new Date(data.list[forecastTime].dt * 1000);
+            var fDay = fDate.getDate();
+            var fMonth = fDate.getMonth() + 1;
+            var fYear = fDate.getFullYear();
+            var forecastInfo = document.createElement("h4");
+                forecastInfo.innerText = fMonth + "/" + fDay + "/" + fYear;
+                forecasts[i].append(forecastInfo);
 
-            document.querySelector(".fDate" + i).innerText = date;
-            document.querySelector(".icon" + i).src = "https://openweathermap.org/img/wn/"+ icon + ".png";
-            document.querySelector(".fTemp" + i).innerText = temp + "°F";
-            document.querySelector(".fWind" + i).innerText = "Wind Speed: " + speed + " mph";
-            document.querySelector(".fHumidity" + i).innerText = "Humidity: " + humidity + "%";
-        };
+        var fIcon = document.createElement("img");
+        fIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastTime].weather[0].icon + "@2x.png");
+        forecasts[i].append(fIcon);
+        
+
+        var fTemp = document.createElement("p");
+        fTemp.innerHTML = "Temp: " + (Math.round(((data.list[forecastTime].main.temp - 273.15) * 9 / 5 + 32) * 100) / 100) + "°F";
+        forecasts[i].append(fTemp);
+        
+
+        var fWind = document.createElement("p");
+        fWind.innerHTML = "Wind: " + (Math.round(2.23694 * data.list[forecastTime].wind.speed * 100) / 100) + " MPH";
+        forecasts[i].append(fWind);
+
+        var fHumidity = document.createElement("p");
+        fHumidity.innerHTML = "Humidity: " + data.list[forecastTime].main.humidity + " %";
+        forecasts[i].append(fHumidity);
+        }
+        
     },
     search2: function () {
         this.fetchForecast(document.querySelector(".search-bar").value);
@@ -115,51 +175,45 @@ let forecast = {
 };
 
 
+//-------------------------------------------------------------------------------------------------------------------//
+// //Recent history load and buttons below 
 
-//-----------------------------------------------------------------------------------------------------------//
+// const searchBar = document.querySelector("#searchBar") //cityInput
+// const submitbtn = document.querySelector("#submit"); //submitBtn
+// const cityRef = document.querySelector("#search-history"); //cityref
+// var city;
+// var cityName;
+// var cities;
 
-
-const searchBar = document.querySelector("#searchBar") //cityInput
-const submitbtn = document.querySelector("#submit"); //submitBtn
-const cityRef = document.querySelector("#search-history"); //cityref
-var city;
-var cityName;
-var cities;
-
-submitbtn.addEventListener("click",searchHistory);
-
-
-function searchHistory() {
-    var newList = document.createElement("li");
-    var recentSearchBtn = document.createElement("button")
-    cityRef.appendChild(newList);
-    newList.appendChild(recentSearchBtn);
-    cityName = searchBar.val;
-    cities = (cityName);
-    recentSearchBtn.textContent = cityName;
-    localStorage.setItem("City",JSON.stringify(cities))
-    localStorage.getItem("City");
+// submitbtn.addEventListener("click",searchHistory);
 
 
-    // document.querySelector(".recent-searches").innerHTML = "";
-    // for (i = 0; i <history.length; i++) {
-    //     var li = document.createElement("li");
-    //     var recentSearchBtn = document.createElement("button");
-    //     recentSearchBtn.innerHTML = history[i];
-    //     recentSearchBtn.append(li);
-    //     recentSearchBtn.append(recentSearchBtn);
-    //     recentSearchBtn.classList.add("recent-search-btn");
+// function searchHistory() {
+//     var newList = document.createElement("li");
+//     var recentSearchBtn = document.createElement("button")
+//     cityRef.appendChild(newList);
+//     newList.appendChild(recentSearchBtn);
+//     cityName = searchBar.val;
+//     cities = (cityName);
+//     recentSearchBtn.textContent = cityName;
+//     localStorage.setItem("City",JSON.stringify(cities))
+//     localStorage.getItem("City");
+// }
 
-        // recentSearchBtn.addEventListener("click", currentWeather)
-    }
+   
 
+//-----------------------------test------------------------------------------------------------------------------------------//
+//For forecast
+// // for (i=0; i<5; i++) {
+        //     // const {date} = data.dt_text;
+        //     // const {icon} = data.weather[0].icon;
+        //     const {temp} = data.main;
+        //     const {humidity} = data.main;
+        //     const {speed} = data.wind;
 
-    /**  fix this **/
-//This is an extra feature that allow you to press enter when you search instead of having to press the search button every time 
-//We add an event listener to "keyup" and give it a function of an "event"
-//"If" the button pressed is equal to "Enter" then run the search function
-// document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-//     if (event.key == "Enter"); {
-//         weather.search();
-//     }
-// });
+        //     document.querySelector(".fDate" + i).innerText = date;
+        //     document.querySelector(".icon" + i).src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        //     document.querySelector(".fTemp" + i).innerText = temp + "°F";
+        //     document.querySelector(".fWind" + i).innerText = "Wind Speed: " + speed + " mph";
+        //     document.querySelector(".fHumidity" + i).innerText = "Humidity: " + humidity + "%";
+        // };
